ffb5c4d4f864b4ad2cf2cb7b0e5d35d3cb5c2924
Justinluu2311 <justinluu2000@gmail.com>
2024-04-02 11:20:04 +0200

    test stuff

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -6,4 +6,4 @@ PROJECT_DIR="$(cd $DIR/..; pwd)"
 ARCH_VERSION=${1:cpu}
 
 docker build --build-arg arch_version=${ARCH_VERSION} --progress=plain -t allrank:latest $PROJECT_DIR
-docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/data/generate_dummy_data.py && python allrank/main.py --config-file-name /allrank/scripts/local_config.json --run-id test_run --job-dir /allrank/test_run'
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name /allrank/scripts/local_config.json --run-id test_run --job-dir /allrank/test_run'
